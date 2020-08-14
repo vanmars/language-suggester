fc20ef58f2152ca1f5b3fac3d2af932744906f6c
Vanessa Stewart <vstewart1213@gmail.com>
2020-08-14 09:25:47 -0700

    Add business logic on scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// Business Logic
+function ruby (num1, num2, num3, num4, num5) {
+  if (Math.round((num1+num2+num3+num4+num5)/5) === 1) {
+    return true;
+  };
+};
+
+function javascript (num1, num2, num3, num4, num5) {
+  if (Math.round((num1+num2+num3+num4+num5)/5) === 2) {
+    return true;
+  };
+};
+
+function python (num1, num2, num3, num4, num5) {
+  if (Math.round((num1+num2+num3+num4+num5)/5) === 3) {
+    return true;
+  };
+};
+
+
+// User Interface Logic
