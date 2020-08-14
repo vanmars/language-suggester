fc2ca822ae0ed3fa48ef8813f7a2ce2d5a8a177c
Vanessa Stewart <vstewart1213@gmail.com>
2020-08-14 09:42:38 -0700

    Add user interface logic to attach event handler to submit event

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,45 @@
 // Business Logic
 function ruby (num1, num2, num3, num4, num5) {
-  if (Math.round((num1+num2+num3+num4+num5)/5) === 1) {
+  if (Math.round((num1 + num2 + num3 + num4 + num5) / 5) === 1) {
     return true;
   };
 };
 
 function javascript (num1, num2, num3, num4, num5) {
-  if (Math.round((num1+num2+num3+num4+num5)/5) === 2) {
+  if (Math.round((num1 + num2 + num3 + num4 + num5) / 5) === 2) {
     return true;
   };
 };
 
 function python (num1, num2, num3, num4, num5) {
-  if (Math.round((num1+num2+num3+num4+num5)/5) === 3) {
+  if (Math.round((num1 + num2 + num3 + num4 + num5) / 5) === 3) {
     return true;
   };
 };
 
 
 // User Interface Logic
+$(document).ready(function() {
+  $("#languageForm").submit(function(event) {
+    event.preventDefault();
+    const companiesInput= parseInt($("#companies").val());
+    const syntaxInput= parseInt($("#syntax").val());
+    const debugInput= parseInt($("#debug").val());
+    const frameworkInput= parseInt($("#framework").val());
+    const adjectiveInput= parseInt($("#adjective").val());
+
+    if (ruby(companiesInput, syntaxInput, debugInput, frameworkInput, adjectiveInput)) {
+      $("#languageForm").hide();
+      $("#result").text("You should study Ruby!");
+    } else if (javascript(companiesInput, syntaxInput, debugInput, frameworkInput, adjectiveInput)){
+      $("#languageForm").hide();
+      $("#result").text("You should study JavaScript!");
+    } else if (python(companiesInput, syntaxInput, debugInput, frameworkInput, adjectiveInput)){
+      $("#languageForm").hide();
+      $("#result").text("You should study Python!");
+    } else {
+      $("#languageForm").hide();
+      $("#result").text("You should study Go!");
+    };
+  });
+});
